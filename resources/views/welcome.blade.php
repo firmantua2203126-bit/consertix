bd07402f0d6e1523cdea1e2c17febff60f8f92fb
fakmonbae <radhinkaka@gmail.com>
2025-11-26 15:39:19 +0700

    fan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -328,7 +328,7 @@
 
 
 
-<!-- Footer t-->
+<!-- Footers t-->
 {{-- <footer class="bg-gray-900 text-white py-8">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
             <p>&copy; {{ date('Y') }} Concertix. All rights reserved.</p>
